01a04b5b6259c64ba66edd31c50d3c2151d16ced
Adalto Picotti Junior <adaltopicotti.jr@gmail.com>
2020-03-27 00:35:08 -0300

    fruits

--- src/controllers/IslandController.js
+++ src/controllers/IslandController.js
@@ -16,41 +16,38 @@ module.exports = {
 
   async store(req, res) {
     const { ownerName, islandName, fruit, hemisphere, turnipPrice, dodoCode} = req.body;
-    let island = await Island.find({ ownerName: ownerName })
-    if(island.length < 1) {
-      switch(fruit.toUpperCas()) {
+    // let island = await Island.find({ ownerName: ownerName })
+    // if(island.length < 1) {
+      switch(fruit.toUpperCase()) {
         case 'APPLE':
-          fruit_url: 'https://firebasestorage.googleapis.com/v0/b/dodo-flights.appspot.com/o/apple.png?alt=media&token=22760cb6-2fbc-4c31-9d1d-4ebcdaac2e50'
+          fruit_url = 'https://firebasestorage.googleapis.com/v0/b/dodo-flights.appspot.com/o/apple.png?alt=media&token=22760cb6-2fbc-4c31-9d1d-4ebcdaac2e50'
           break;
         case 'CHERRY':
-          fruit_url: ''
+          fruit_url = 'https://firebasestorage.googleapis.com/v0/b/dodo-flights.appspot.com/o/cherry.png?alt=media&token=73d3ff4f-b517-4c89-8ff5-0d709e7da995'
           break;
         case 'ORANGE':
-          fruit_url: ''
+          fruit_url = 'https://firebasestorage.googleapis.com/v0/b/dodo-flights.appspot.com/o/orange.png?alt=media&token=7b4b8e49-c601-438d-baf4-fb7b0237ef7a'
           break;
         case 'PEACH':
-          fruit_url: ''
+          fruit_url = 'https://firebasestorage.googleapis.com/v0/b/dodo-flights.appspot.com/o/peach.png?alt=media&token=3b855f4a-607e-4a57-83d3-68bc1a3048fd'
           break;
         case 'PEAR':
-          fruit_url: ''
-          break;
-        case y:
-          // code block
+          fruit_url = 'https://firebasestorage.googleapis.com/v0/b/dodo-flights.appspot.com/o/pear.png?alt=media&token=4fa4931f-be40-45c1-a981-74d9df5a486e'
           break;
         default:
-          // code block
       }
-      island = await Island.create({
+      const island = await Island.create({
         ownerName,
         islandName,
         fruit,
+        fruit_url,
         hemisphere,
         turnipPrice,
         dodoCode,
         createdAt: new Date(Date.now()).toLocaleString()
       });
 
-    }
+    // }
 
     return res.json(island);
   }
