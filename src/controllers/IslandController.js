68bc0d6f36b704857fe8059f76dc2b35ab7e9334
Adalto Picotti Junior <adaltopicotti.jr@gmail.com>
2020-03-27 00:22:31 -0300

    fruit

--- src/controllers/IslandController.js
+++ src/controllers/IslandController.js
@@ -18,6 +18,28 @@ module.exports = {
     const { ownerName, islandName, fruit, hemisphere, turnipPrice, dodoCode} = req.body;
     let island = await Island.find({ ownerName: ownerName })
     if(island.length < 1) {
+      switch(fruit.toUpperCas()) {
+        case 'APPLE':
+          fruit_url: 'https://firebasestorage.googleapis.com/v0/b/dodo-flights.appspot.com/o/apple.png?alt=media&token=22760cb6-2fbc-4c31-9d1d-4ebcdaac2e50'
+          break;
+        case 'CHERRY':
+          fruit_url: ''
+          break;
+        case 'ORANGE':
+          fruit_url: ''
+          break;
+        case 'PEACH':
+          fruit_url: ''
+          break;
+        case 'PEAR':
+          fruit_url: ''
+          break;
+        case y:
+          // code block
+          break;
+        default:
+          // code block
+      }
       island = await Island.create({
         ownerName,
         islandName,
--- src/models/Island.js
+++ src/models/Island.js
@@ -4,19 +4,20 @@ const IslandSchema = new mongoose.Schema({
   ownerName: String,
   islandName: String,
   fruit: String,
+  fruit_url: String,
   hemisphere: String,
   turnipPrice: Number,
   dodoCode: String,
   createdAt: String
 }, {
   collation: { locale: 'en', strength: 2},
-  toJSON: {
-    virtuals: true
-  }
+  // toJSON: {
+  //   virtuals: true
+  // }
 });
 
-IslandSchema.virtual('fruit_url').get(function() {
-  return `http://flights.riddlecode.com/files/${this.fruit}.png`
-});
+// IslandSchema.virtual('fruit_url').get(function() {
+//   return `http://flights.riddlecode.com/files/${this.fruit}.png`
+// });
 
 module.exports = mongoose.model('Island', IslandSchema);
