c28e1db1c48a17e6134dca501755d194aed78d2e
Adalto Picotti Junior <adaltopicotti.jr@gmail.com>
2020-08-11 11:42:14 -0300

    Update jwt.js

--- jwt.js
+++ jwt.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
-const secret = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9';
-
+//test const secret = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9';
+const secret = ENV.secret
 // Date start: 1970
 // Each day is exactly 86400 seconds
 module.exports = {
